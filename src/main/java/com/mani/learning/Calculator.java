ad4bc451243311c182efc5f59718a571d125cbe3
Manikanta Kathula <kathula.manikanta@gmail.com>
2017-08-02 13:54:25 -0500

    Cobertura

--- src/main/java/com/mani/learning/Calculator.java
+++ src/main/java/com/mani/learning/Calculator.java
@@ -6,16 +6,16 @@ public class Calculator<T> {
 
 	private T operator1, operator2;
 
-	/** Empty constructor 
-	 * for the creation
-	 *  of Calculator object. */
+	/**
+	 * Empty constructor for the creation of Calculator object.
+	 */
 	public Calculator() {
-
 	}
+
 	/** Calculator constructor with operands. hellow */
 	public Calculator(final T operator1, final T operator2) {
-		this.operator1 = operator1;
-		this.operator2 = operator2;
+		this();
+		setOperator2(operator1, operator2);
 	}
 
 	public void setOperator2(final T operator1, final T operator2) {
--- src/test/java/com/mani/learning/CalculatorTest.java
+++ src/test/java/com/mani/learning/CalculatorTest.java
@@ -5,23 +5,29 @@ import org.junit.Test;
 
 public class CalculatorTest {
 
-	Calculator<Integer> calc = new Calculator<Integer>(2,5);
+	Calculator<Integer> calc = new Calculator<Integer>(2, 5);
+
 	@Test
-	public void testAdd(){
-		
-		Assert.assertTrue(calc.operation((u,v)->u+v)==7);
+	public void testOperands() {
+		Assert.assertTrue(calc.getOperator1()==2);
+		Assert.assertTrue(calc.getOperator2()==5);
 	}
-	
+
 	@Test
-	public void testMultiply(){
-		
-		Assert.assertTrue(calc.operation((u,v)->u*v)==10);
+	public void testAdd() {
+
+		Assert.assertTrue(calc.operation((u, v) -> u + v) == 7);
 	}
-	
-	
+
 	@Test
-	public void testDivision(){
-		
-		Assert.assertTrue(calc.operation((u,v)->u/v)==0);
+	public void testMultiply() {
+
+		Assert.assertTrue(calc.operation((u, v) -> u * v) == 10);
+	}
+
+	@Test
+	public void testDivision() {
+
+		Assert.assertTrue(calc.operation((u, v) -> u / v) == 0);
 	}
 }
